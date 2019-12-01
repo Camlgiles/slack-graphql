202193c1c80ad68d62c2297db12a7fd8781193d5
Camlgiles <cgiles@gilescpa.com>
2019-11-30 16:06:43 -0800

    merge slack-client repo

---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
+import {
+  ApolloClient,
+  createNetworkInterface,
+  ApolloProvider
+} from "react-apollo";
+import "semantic-ui-css/semantic.min.css";
+
+const networkInterface = createNetworkInterface({
+  uri: "http://localhost:8080/graphql"
+});
+
+const client = new ApolloClient({
+  networkInterface: networkInterface
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById("root"));
